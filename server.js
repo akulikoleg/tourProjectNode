1eb3db103b7b768590c16b7c7987f2a46d035780
Oleg Aculov <oaculov@gmail.com>
2024-10-21 21:34:44 -0400

    reinitializing project

---
+++ server.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNGANDLER REJECTION! Shutting down...');
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    autoIndex: true,
+    // useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+//console.log(process.env);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on Port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
